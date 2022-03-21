a27b8d15b8adb24fe8b86bf09db6fb9b84f4ea3e
windy82581 <windy82581@gmail.com>
2022-03-21 17:09:06 +0900

    0321

--- 0320 프로젝트 연습/testing2/settings.py
+++ 0320 프로젝트 연습/testing2/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2lo4@v&kzy_kedk=rl*rbasiv+*lkw6e+u4t0s2jz%x*y9_!7s'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
